ddd566d8e85544aea4a0f9f441783c0615280ea5
Chris Young <novamation@gmail.com>
2023-02-18 14:31:38 -0500

    Fixed error in getMatch

--- backend/src/utilities/getMatch.ts
+++ backend/src/utilities/getMatch.ts
@@ -1,5 +1,5 @@
 import { AnyObj } from '../types/AnyObj';
 
 export function getMatch(single: AnyObj, group: AnyObj[], key: string) {
-  return group.find((entry) => (entry[key] = single[key]));
+  return group.find((entry) => entry[key] === single[key]);
 }
