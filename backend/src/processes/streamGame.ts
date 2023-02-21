ab0beae830b2032c8f65fd33f3125182735ba242
Chris Young <novamation@gmail.com>
2023-02-21 17:18:40 -0500

    Some try catches added.

--- backend/src/processes/streamGame.ts
+++ backend/src/processes/streamGame.ts
@@ -18,26 +18,54 @@ export function streamGame(
     )
     .subscribe({
       next: async (freshGame) => {
-        const storedActiveGame = await activeGameService.findByNhlId(
-          activeGame.nhlId,
-        );
+        let storedActiveGame;
+        let storedGame;
 
-        await activeGameService.update(storedActiveGame.id, {
-          status: freshGame.status,
-          lastUpdated: freshGame.timeString,
-        });
+        try {
+          storedActiveGame = await activeGameService.findByNhlId(
+            activeGame.nhlId,
+          );
+        } catch (err) {
+          console.error(
+            'Unable to find activeGame with nhlId ' + activeGame.nhlId,
+          );
+          throw new Error(err);
+        }
 
-        const storedGame = await gameService.findByNhlId(activeGame.nhlId);
-        if (
-          storedGame.homeScore != freshGame.home ||
-          storedGame.awayScore != freshGame.away ||
-          storedGame.status != freshGame.status
-        ) {
-          await gameService.update(storedGame.id, {
-            homeScore: freshGame.home,
-            awayScore: freshGame.away,
+        try {
+          await activeGameService.update(storedActiveGame.id, {
             status: freshGame.status,
+            lastUpdated: freshGame.timeString,
           });
+        } catch (err) {
+          console.error(
+            'Unable to update activeGame with id ' + storedActiveGame.id,
+          );
+          throw new Error(err);
+        }
+
+        try {
+          storedGame = await gameService.findByNhlId(activeGame.nhlId);
+        } catch (err) {
+          console.error('Cannot find game with nhlId ' + activeGame.nhlId);
+          throw new Error(err);
+        }
+
+        try {
+          if (
+            storedGame.homeScore != freshGame.home ||
+            storedGame.awayScore != freshGame.away ||
+            storedGame.status != freshGame.status
+          ) {
+            await gameService.update(storedGame.id, {
+              homeScore: freshGame.home,
+              awayScore: freshGame.away,
+              status: freshGame.status,
+            });
+          }
+        } catch (err) {
+          console.error('Cannot update game with id ' + storedGame.id);
+          throw new Error(err);
         }
 
         if (['5', '6', '7'].includes(freshGame.status)) {
